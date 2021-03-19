29fb414abc6222b1f56cd87e57472ff82d4ec5a3
gregor-gh <gregor@live.co.uk>
2021-03-19 20:33:37 +0000

    Completed draw_line_plot

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -5,22 +5,25 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = None
-
+df = pd.read_csv("fcc-forum-pageviews.csv", parse_dates=[0], index_col=0)
 # Clean data
-df = None
+df = df.loc[(df['value'] >= df['value'].quantile(0.025)) &
+            (df['value'] <= df['value'].quantile(0.975))]
+#df = df.loc[df['value'] <= df['value'].quantile(0.975)]
 
 
 def draw_line_plot():
     # Draw line plot
+    g = df.plot(kind="line")
+    g.set_title("Daily freeCodeCamp Forum Page Views 5/2016-12/2019")
+    g.set_xlabel("Date")
+    g.set_ylabel("Page Views")
 
-
-
-
-
+    fig = g.figure
     # Save image and return fig (don't change this part)
     fig.savefig('line_plot.png')
     return fig
+
 
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
@@ -28,13 +31,10 @@ def draw_bar_plot():
 
     # Draw bar plot
 
-
-
-
-
     # Save image and return fig (don't change this part)
-    fig.savefig('bar_plot.png')
-    return fig
+    # fig.savefig('bar_plot.png')
+    # return fig
+
 
 def draw_box_plot():
     # Prepare data for box plots (this part is done!)
@@ -45,10 +45,9 @@ def draw_box_plot():
 
     # Draw box plots (using Seaborn)
 
-
-
-
-
     # Save image and return fig (don't change this part)
-    fig.savefig('box_plot.png')
-    return fig
+    # fig.savefig('box_plot.png')
+    # return fig
+
+
+draw_line_plot()
